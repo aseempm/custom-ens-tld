f9ba989b1c67add00b6ac417a6a3610911622865
Aseem PM <aseempm@protonmail.com>
2024-03-10 00:50:54 +0530

    updated the deploy script

--- scripts/01_deploy.ts
+++ scripts/01_deploy.ts
@@ -1,19 +1,54 @@
 import { ethers } from "hardhat";
+import { labelhash, namehash, zeroHash } from "viem";
 
 async function main() {
-  const tld = ethers.encodeBytes32String(".aseem");
-  const zero = ethers.encodeBytes32String("");
+  const [deployer] = await ethers.getSigners();
+  const TLD = namehash("aseem");
 
-  const ens = await ethers.deployContract("ENSRegistry");
-  await ens.waitForDeployment();
-  console.log("ENS", ens.target);
+  const registry = await ethers.deployContract("ENSRegistry");
+  await registry.waitForDeployment();
+  console.log("ENSResigtry", registry.target);
 
-  const myTLD = await ethers.deployContract("CustomTLD", [tld, ens.target]);
-  await myTLD.waitForDeployment();
-  console.log("TLD", myTLD.target);
+  const root = await ethers.deployContract("Root", [registry.target]);
+  await root.waitForDeployment();
+  console.log("Root", root.target);
 
-  await ens.setOwner(zero, myTLD.target);
-  await myTLD.registerTLD();
+  const registrar = await ethers.deployContract("BaseRegistrarImplementation", [
+    registry.target,
+    TLD,
+  ]);
+  await registrar.waitForDeployment();
+  console.log("Registrar", registrar.target);
+
+  const reverseRegistrar = await ethers.deployContract("ReverseRegistrar", [
+    registry.target,
+  ]);
+  await reverseRegistrar.waitForDeployment();
+  console.log("ReverseRegistrar", reverseRegistrar.target);
+
+  const resolver = await ethers.deployContract("CustomResolver", [
+    registry.target,
+    reverseRegistrar.target,
+  ]);
+  await resolver.waitForDeployment();
+  console.log("Resolver", resolver.target);
+
+  await registry.setOwner(zeroHash, root.target);
+
+  await root.setController(deployer, true);
+  await root.setSubnodeOwner(labelhash("aseem"), registrar);
+  await root.setSubnodeOwner(labelhash("reverse"), deployer);
+
+  await registry.setSubnodeOwner(
+    namehash("reverse"),
+    labelhash("addr"),
+    reverseRegistrar.target
+  );
+
+  await reverseRegistrar.setDefaultResolver(resolver.target);
+
+  await registrar.addController(deployer);
+  await registrar.setResolver(resolver.target);
 }
 
 main()
